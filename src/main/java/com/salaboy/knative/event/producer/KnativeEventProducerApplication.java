201c10d98c1cdd67d7d04445b3696af55e363bcf
salaboy <salaboy@gmail.com>
2020-08-19 10:41:20 +0100

    event producer for broker

--- src/main/java/com/salaboy/knative/event/producer/KnativeEventProducerApplication.java
+++ src/main/java/com/salaboy/knative/event/producer/KnativeEventProducerApplication.java
@@ -2,11 +2,13 @@ package com.salaboy.knative.event.producer;
 
 import com.salaboy.cloudevents.helper.CloudEventsHelper;
 import io.cloudevents.CloudEvent;
-import io.cloudevents.v03.AttributesImpl;
-import io.cloudevents.v03.CloudEventBuilder;
+import io.cloudevents.core.builder.CloudEventBuilder;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.SerializationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,31 +16,31 @@ import reactor.core.publisher.Mono;
 
 import java.net.URI;
 import java.util.UUID;
-import java.util.logging.Logger;
+
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class KnativeEventProducerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KnativeEventProducerApplication.class, args);
     }
 
-    private final static Logger logger = Logger.getLogger(KnativeEventProducerApplication.class.getName());
 
-    @Value("${SINK_HOST:localhost}")
+    @Value("${K_SINK:localhost}")
     private String HOST;
 
 
     @GetMapping("/{name}")
     public String doSomethingAndSendCloudEvent(@PathVariable("name") String name) {
 
-        final CloudEvent<AttributesImpl, String> myCloudEvent = CloudEventBuilder.<String>builder()
+        final CloudEvent myCloudEvent = CloudEventBuilder.v1()
                 .withId("ABC-123")
-                .withType("my-first-cloud-event")
+                .withType("MyCloudEvent.JustHappened")
                 .withSource(URI.create("knative-event-producer.default.svc.cluster.local"))
-                .withData("{\"name\" : \"" + name + "-" + UUID.randomUUID().toString() + "\" }")
-                .withDatacontenttype("application/json")
+                .withData(SerializationUtils.serialize("{\"name\" : \"" + name + "-" + UUID.randomUUID().toString() + "\" }"))
+                .withDataContentType("application/json")
                 .build();
 
         WebClient webClient = WebClient.builder().baseUrl(HOST).filter(logRequest()).build();
@@ -54,8 +56,8 @@ public class KnativeEventProducerApplication {
 
     private static ExchangeFilterFunction logRequest() {
         return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
-            logger.info("Request: " + clientRequest.method() + " - " + clientRequest.url());
-            clientRequest.headers().forEach((name, values) -> values.forEach(value -> logger.info(name + "=" + value)));
+            log.info("Request: " + clientRequest.method() + " - " + clientRequest.url());
+            clientRequest.headers().forEach((name, values) -> values.forEach(value -> log.info(name + "=" + value)));
             return Mono.just(clientRequest);
         });
     }
